87fc5876d7ff4892167866c751defa0226af4f44
kavya439 <kavyanjalipurini123@gmail.com>
2026-04-25 12:42:35 +0530

    Create 02_insert_data.sql

---
+++ 02_insert_data.sql
@@ -0,0 +1,11 @@
+-- Insert sample employees
+INSERT INTO employees (emp_name, department, salary)
+VALUES ('Ravi Kumar', 'IT', 55000);
+
+INSERT INTO employees (emp_name, department, salary)
+VALUES ('Priya Sharma', 'HR', 45000);
+
+INSERT INTO employees (emp_name, department, salary)
+VALUES ('Arun Reddy', 'Finance', 60000);
+
+COMMIT;
